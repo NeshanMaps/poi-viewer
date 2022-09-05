ba62b4c6f2f646c7d08a64167c442a45fba23d41
amir.barband <amir1368431>
2022-09-05 18:14:49 +0430

    Initial commit

---
+++ app/src/main/java/org/neshan/poiviewer/MainActivity.java
@@ -0,0 +1,165 @@
+package org.neshan.poiviewer;
+
+import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.carto.styles.AnimationStyle;
+import com.carto.styles.AnimationStyleBuilder;
+import com.carto.styles.AnimationType;
+import com.carto.styles.MarkerStyle;
+import com.carto.styles.MarkerStyleBuilder;
+
+import org.neshan.common.model.LatLng;
+import org.neshan.mapsdk.MapView;
+import org.neshan.mapsdk.internal.utils.BitmapUtils;
+import org.neshan.mapsdk.model.Marker;
+import org.neshan.poiviewer.adapter.SearchAdapter;
+import org.neshan.servicessdk.search.NeshanSearch;
+import org.neshan.servicessdk.search.model.Item;
+import org.neshan.servicessdk.search.model.NeshanSearchResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class MainActivity extends AppCompatActivity implements SearchAdapter.OnSearchItemListener {
+
+    private static final String BAKERY = "نانوایی";
+    private static final String MOSQUE = "مسجد";
+    private static final String ATM = "atm";
+
+    private MapView mapView;
+    private Button btnMosques;
+    private Button btnAtms;
+    private Button btnBakery;
+
+    private List<Item> items;
+    private SearchAdapter adapter;
+    private Runnable runnable;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mapView = findViewById(R.id.mapview);
+        btnMosques = findViewById(R.id.btn_mosque);
+        btnAtms = findViewById(R.id.btn_atms);
+        btnBakery = findViewById(R.id.btn_bakery);
+
+        items = new ArrayList<>();
+        adapter = new SearchAdapter(items, this);
+
+        btnMosques.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search(mapView.getCameraTargetPosition(), MOSQUE);
+            }
+        });
+
+        btnAtms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search(mapView.getCameraTargetPosition(), ATM);
+            }
+        });
+
+        btnBakery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                search(mapView.getCameraTargetPosition(), BAKERY);
+            }
+        });
+    }
+
+    private void search(LatLng searchPosition, String text) {
+        //TODO: Replace YOUR-API-KEY with your api key
+        new NeshanSearch.Builder("YOUR-API-KEY")
+                .setLocation(searchPosition)
+                .setTerm(text)
+                .build().call(new Callback<NeshanSearchResult>() {
+                    @Override
+                    public void onResponse(Call<NeshanSearchResult> call, retrofit2.Response<NeshanSearchResult> response) {
+                        if (response.code() == 403) {
+                            Toast.makeText(MainActivity.this, getString(R.string.connection_error), Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        if (response.body() != null) {
+                            mapView.clearMarkers();
+                            NeshanSearchResult result = response.body();
+                            items = result.getItems();
+                            for (Item item : items) {
+                                if (text.equals(MOSQUE) && item.getType().equals("mosque")) {
+                                    mapView.addMarker(createMarker(new LatLng(item.getLocation().getLatitude(), item.getLocation().getLongitude()), R.drawable.mosque));
+                                }
+                                if (text.equals(ATM) && item.getType().equals("atm")) {
+                                    mapView.addMarker(createMarker(new LatLng(item.getLocation().getLatitude(), item.getLocation().getLongitude()), R.drawable.atm));
+                                }
+                                if (text.equals(BAKERY) && item.getType().equals("bakery")) {
+                                    mapView.addMarker(createMarker(new LatLng(item.getLocation().getLatitude(), item.getLocation().getLongitude()), R.drawable.bakery));
+                                }
+                            }
+                            if (items != null && !items.isEmpty() && items.get(0) != null) {
+                                mapView.moveCamera(new LatLng(items.get(0).getLocation().getLatitude(), items.get(0).getLocation().getLongitude()), .5f);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<NeshanSearchResult> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, getString(R.string.connection_error), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    @Override
+    public void onSeachItemClick(LatLng latLng) {
+        closeKeyBoard();
+        adapter.updateList(new ArrayList<Item>());
+        mapView.setZoom(16f, 0);
+        mapView.moveCamera(latLng, 0);
+    }
+
+    private void closeKeyBoard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (getCurrentFocus() != null) {
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
+    private Marker createMarker(LatLng loc, int markerDrawable) {
+        // Creating animation for marker. We should use an object of type AnimationStyleBuilder, set
+        // all animation features on it and then call buildStyle() method that returns an object of type
+        // AnimationStyle
+        AnimationStyleBuilder animStBl = new AnimationStyleBuilder();
+        animStBl.setFadeAnimationType(AnimationType.ANIMATION_TYPE_SMOOTHSTEP);
+        animStBl.setSizeAnimationType(AnimationType.ANIMATION_TYPE_SPRING);
+        animStBl.setPhaseInDuration(0.5f);
+        animStBl.setPhaseOutDuration(0.5f);
+        AnimationStyle animSt = animStBl.buildStyle();
+
+        // Creating marker style. We should use an object of type MarkerStyleCreator, set all features on it
+        // and then call buildStyle method on it. This method returns an object of type MarkerStyle
+        MarkerStyleBuilder markStCr = new MarkerStyleBuilder();
+        markStCr.setSize(20f);
+//        markStCr.setAnchorPoint(-.1f,.3f);
+        markStCr.setBitmap(BitmapUtils.createBitmapFromAndroidBitmap(BitmapFactory.decodeResource(getResources(), markerDrawable)));
+        // AnimationStyle object - that was created before - is used here
+        markStCr.setAnimationStyle(animSt);
+        MarkerStyle markSt = markStCr.buildStyle();
+
+        // Creating marker
+        return new Marker(loc, markSt);
+    }
+}
---
+++ app/src/main/java/org/neshan/poiviewer/adapter/SearchAdapter.java
@@ -0,0 +1,75 @@
+package org.neshan.poiviewer.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatTextView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.neshan.common.model.LatLng;
+import org.neshan.poiviewer.R;
+import org.neshan.servicessdk.search.model.Item;
+import org.neshan.servicessdk.search.model.Location;
+
+import java.util.List;
+
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
+
+    private List<Item> items;
+    private OnSearchItemListener onSearchItemListener;
+
+    public SearchAdapter(List<Item> items, OnSearchItemListener onSearchItemListener) {
+        this.items = items;
+        this.onSearchItemListener = onSearchItemListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_search, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.tvTitle.setText(items.get(position).getTitle());
+        holder.tvAddress.setText(items.get(position).getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public void updateList(List<Item> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private AppCompatTextView tvTitle;
+        private AppCompatTextView tvAddress;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.textView_title);
+            tvAddress = itemView.findViewById(R.id.textView_address);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Location location = items.get(getAdapterPosition()).getLocation();
+            LatLng LatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            onSearchItemListener.onSeachItemClick(LatLng);
+        }
+    }
+
+    public interface OnSearchItemListener {
+        void onSeachItemClick(LatLng latLng);
+    }
+}
